4c16ca898f9d89343a1abef03afd13d5038e3f95
zibi <zibi@nora.pl>
2018-11-02 11:20:49 +0100

    zx print inv

--- zx1/zx_func.asm
+++ zx1/zx_func.asm
@@ -28,6 +28,39 @@ zx_print_func_loop:
 	
 	jsr zx_print_func
 .ENDM
+
+zx_print_inv_func:
+	ldx #8
+	ldy #0
+zx_print_inv_func_loop:
+	lda (src),y
+	eor #$ff
+	sta (dst),y
+	INW src
+	ADW dst #32
+	dex
+	bne zx_print_inv_func_loop
+
+	rts
+	
+.MACRO ZX_PRINT_INV letter position
+	mwa #image_data_zx_font src
+	mwa #screen_mem_zx dst
+
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+	ADW src #:letter
+
+	ADW dst #:position
+	
+	jsr zx_print_inv_func
+.ENDM
+	
 	
 	
 		
